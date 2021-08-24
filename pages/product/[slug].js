12c71adce1f58a4f7ee73a5bb7bfa1c3faadec8d
hajzeer <krzysh98@gmail.com>
2021-08-24 21:03:02 +0200

    fix

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -470,7 +470,7 @@ export const getStaticPaths = async () => {
     return { paths, fallback: false };
 };
 
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
     const { slug: Slug } = context.params;
     const { data } = await client.query({
         query: GET_PRODUCTS_DETAILS,
